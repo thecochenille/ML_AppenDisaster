06063c6728c339948c01e62c5a9288f1d37cb505
Isabelle M. Vea <isabelle.vea@gmail.com>
2023-06-13 16:00:35 -0500

    added files for the app + edited train_classifier
    - train_classifier: replaced model.pkl by the function path
    - added html templates

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -92,7 +92,7 @@ def evaluate_model(model, X_test, Y_test, category_names):
 
 
 def save_model(model, model_filepath):
-    with open('model.pkl', 'wb') as file:
+    with open(model_filepath, 'wb') as file:
         pickle.dump(model, file)
 
 
